40cbbd87b9bd100d27624b77002978788f67b78a
Ian Mustafa <cuplizian@gmail.com>
2019-09-18 18:38:27 +0700

    Create first test case and first failing test

---
+++ tests/Feature/ManageTaskTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ManageTaskTest extends TestCase
+{
+    /** @test */
+    public function test_user_can_create_a_task()
+    {
+        // User membuka halaman "All Tasks"
+        $this->visit('/tasks');
+
+        // User mengisi isian `name` dan `description`, lalu klik "Create Task"
+        $this->submitForm('Create Task', [
+            'name' => 'My First Task',
+            'description' => 'This is my first task in my new job.',
+        ]);
+
+        // Pastikan task berhasil tersimpan di database
+        $this->seeInDatabase('tasks', [
+            'name' => 'My First Task',
+            'description' => 'This is my first task in my new job.',
+            'is_done' => 0,
+        ]);
+
+        // Redirect ke halaman "All Tasks"
+        $this->seePageIs('/tasks');
+
+        // User dapat melihat task yang baru saja ditambahkan
+        $this->see('My First Task');
+        $this->see('This is my first task in my new job.');
+    }
+
+    /** @test */
+    public function test_user_can_read_all_tasks()
+    {
+        $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function test_user_can_edit_an_existing_task()
+    {
+        $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function test_user_can_delete_an_existing_task()
+    {
+        $this->assertTrue(true);
+    }
+}
