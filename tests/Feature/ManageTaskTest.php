1051314672acbb3c70ecdba2efa2624711f097e2
Ian Mustafa <cuplizian@gmail.com>
2019-09-20 16:44:10 +0700

    Refactor ManageTaskTest

--- tests/Feature/ManageTaskTest.php
+++ tests/Feature/ManageTaskTest.php
@@ -13,26 +13,21 @@ class ManageTaskTest extends TestCase
     /** @test */
     public function test_user_can_create_a_task()
     {
-        // User membuka halaman "All Tasks"
         $this->visit('/tasks');
 
-        // User mengisi isian `name` dan `description`, lalu klik "Create Task"
         $this->submitForm('Create Task', [
             'name' => 'My First Task',
             'description' => 'This is my first task in my new job.',
         ]);
 
-        // Pastikan task berhasil tersimpan di database
         $this->seeInDatabase('tasks', [
             'name' => 'My First Task',
             'description' => 'This is my first task in my new job.',
             'is_done' => 0,
         ]);
 
-        // Redirect ke halaman "All Tasks"
         $this->seePageIs('/tasks');
 
-        // User dapat melihat task yang baru saja ditambahkan
         $this->see('My First Task');
         $this->see('This is my first task in my new job.');
     }
